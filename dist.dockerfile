4dd3b0c339119d4668fc50bff9b797a0a5736776
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2020-10-24 21:44:54 +0800

    解决前端获取后端api固定的毛病

--- dist.dockerfile
+++ dist.dockerfile
@@ -1,4 +1,7 @@
 FROM nginx:stable-alpine
+WORKDIR /usr/share/nginx/html
 COPY dist /usr/share/nginx/html
 EXPOSE 80
+COPY entrypoint.sh .
+ENTRYPOINT ["entrypoint.sh"]
 CMD ["nginx", "-g", "daemon off;"]
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#解决服务器端ip地址固定的bug
+ip=$(curl -4 ip.sb)
+file=`ls *.js`;
+echo $file
+for item in $file
+do
+    filename=${item%.*}
+    echo $filename
+    sed -i "s/127.0.0.1/'$ip'/g" $item
+done
+# ip=1.1.1.1
+# sed -i "s/127.0.0.1/'$ip'/g" test.js
