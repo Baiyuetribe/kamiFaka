b480003a7d58dcc834ef5f438541d74e05b28546
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2020-10-25 16:45:36 +0800

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #解决服务器端ip地址固定的bug
 ip=$(curl -4 ip.sb)
+cd /usr/share/nginx/html/js
 file=`ls *.js`;
 echo $file
 for item in $file
@@ -11,3 +12,7 @@ do
 done
 # ip=1.1.1.1
 # sed -i "s/127.0.0.1/'$ip'/g" test.js
+
+echo Serving application
+
+nginx -g 'daemon off
