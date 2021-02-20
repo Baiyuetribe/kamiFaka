a27244062c7086297c260b26ab3d9314f09a31a4
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2021-02-20 19:41:24 +0800

    随机key安全更新

--- init_mysql.py
+++ init_mysql.py
@@ -5,6 +5,9 @@ from service.api.db import db
 # print(os.getenv('MYSQL_PORT'))
 # print(os.getenv('MYSQL_PASSWORD'))
 # 初始化数据
+import re
+import random
+import string
 
 def new_table():
     # 清空表
@@ -25,23 +28,33 @@ def init():
         res = False
     if res:
         print('检测到已存在数据库')
-        pass
     else:
         try:
+            # 改key
+            mod_key()
             new_table()
             print('新数据库初始化完成')   
         except Exception as e:
             print(e)
             print('初始化失败,请检查数据库设置、防火墙、以及是否初始化完成')
-    # try:
-    #     if bool(AdminUser.query.filter_by(id = 1).first()):
-    #         print('检测到已存在数据库')
-    #         pass
-    #     else:
-    #         new_table()
-    #         print('新数据库初始化完成')    
-    # except:
-    #     print('初始化失败,请检查数据库设置、防火墙、以及是否初始化完成')
+
+def ranstr(num):
+    return ''.join(random.sample(string.ascii_letters + string.digits, num))
+
+def mod_key():
+    w_str = ''
+    randkey = ranstr(random.randint(44,48))
+    flag = False
+    with open('./service/api/db.py','r',encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            if re.search('a44545de51d5e4deaswdedcecvrcrfr5f454fd1cec415r4f',line):
+                line = re.sub('a44545de51d5e4deaswdedcecvrcrfr5f454fd1cec415r4f',randkey,line)
+                flag = True
+            w_str += line
+    if flag:        
+        with open('./service/api/db.py','w',encoding='utf-8') as f:
+            f.write(w_str)
 
 init()
 
