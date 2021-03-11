797b5580349adc9d29dd543565eda87e7cfe8141
佰阅部落 <43716063+Baiyuetribe@users.noreply.github.com>
2021-03-11 16:45:41 +0800

    update

--- service/util/order/handle.py
+++ service/util/order/handle.py
@@ -17,21 +17,25 @@ from service.util.log import log
 
 def notify_success(out_order_id):
     # 更新状态
-    res = TempOrder.query.filter_by(out_order_id = out_order_id,status = False).first()
-    if res:
-        # 生成实际卡密
-        name = res.name
-        payment = res.payment
-        contact = res.contact
-        contact_txt = res.contact_txt
-        price = res.price
-        num = res.num
-        total_price = res.total_price
-        auto = res.auto        
-        with db.auto_commit_db():   # 更新卡密状态
-            TempOrder.query.filter_by(out_order_id = out_order_id).update({'status':True})
+    try:
+        res = TempOrder.query.filter_by(out_order_id = out_order_id,status = False).first()
+        if res:
+            # 生成实际卡密
+            name = res.name
+            payment = res.payment
+            contact = res.contact
+            contact_txt = res.contact_txt
+            price = res.price
+            num = res.num
+            total_price = res.total_price
+            auto = res.auto        
+            with db.auto_commit_db():   # 更新卡密状态
+                TempOrder.query.filter_by(out_order_id = out_order_id).update({'status':True})
 
-        make_order(out_order_id,name,payment,contact,contact_txt,price,num,total_price,auto)
+            make_order(out_order_id,name,payment,contact,contact_txt,price,num,total_price,auto)
+    except Exception as e:
+        print(e)
+        log(e)
 
 #创建订单--走数据库
 def make_order(out_order_id,name,payment,contact,contact_txt,price,num,total_price,auto):
